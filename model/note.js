49d7e506cf9f3b56e07b2add133de1642ecd775a
ergouwang <330035385@qq.com>
2020-07-06 21:22:39 +0800

    成功从数据库中获取所有notes

--- model/note.js
+++ model/note.js
@@ -1,6 +1,7 @@
+const path = require('path')
 const  Sequelize = require('sequelize');
 
-let sequelize = new  Sequelize(undefined, undefined, undefined, { host: 'localhost', dialect: 'sqlite', storage: '../database/database.sqlite' });
+let sequelize = new  Sequelize(undefined, undefined, undefined, { host: 'localhost', dialect: 'sqlite', storage: path.join(__dirname,'../database/database.sqlite')});
 
 //测试连接是否建立
 // sequelize
@@ -9,20 +10,22 @@ let sequelize = new  Sequelize(undefined, undefined, undefined, { host: 'localho
 //     .catch(err => { console.error('Unable to connect to the database:', err); });
 
 //创建 Note 模型，一个模型对应到数据库里面就是一个表
-let  Note = sequelize.define('note', {
-    text: {
-        type: Sequelize.STRING,
-        field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
-    },
-}, {
-    freezeTableName: true // Model tableName will be the same as the model name
-});
+let  Note = sequelize.define('note', { text: { type: Sequelize.STRING, }, });
 
-Note.sync().then(function () {
-    return Note.create({ text: '我要回家', });
-});
+//看一下数据库中是否存在此表，如果不存在则创建此表并写入数据
+// Note.sync().then(function () {
+//     Note.create({ text: '我要回家', });
+// }).then(function () {
+//     //raw 获取数据库中的原始数据
+//     Note.findAll({raw:true}).then(function (notes) {
+//         console.log(notes);
+//     });
+// });
 
-Note.findAll().then(function (notes) {
-    console.log(notes);
-});
+//查询id=2的数据
+// Note.findAll({raw:true,where:{id:2}}).then(function (notes) {
+//     console.log(notes);
+// })
+
+module.exports = Note
 
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,13 @@
 let express = require('express')
 let router = express.Router()
+const Note = require('../model/note')
 
 
-router.get('/notes', function (req, res, next) {
-    console.log('notes 1111111')
-    res.send('respond with a resource')
+router.get('/notes', function (request, response, next) {
+    Note.findAll({raw:true}).then(function (notes) {
+        response.send({status:0,data:notes})
+    })
+
 })
 
 router.post('/notes/add', function (req, res, next) {
