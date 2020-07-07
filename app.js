a8d80f15ef42ba8f414d85ecc00edc0a40d277b5
ergouwang <330035385@qq.com>
2020-07-07 08:16:05 +0800

    后台功能基本完成

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 let  api = require('./routes/api');
+let auth = require('./routes/auth')
 
 
 let app = express();
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api',api)
+app.use('/auth',auth)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
